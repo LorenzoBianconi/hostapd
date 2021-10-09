858397eae85ff70abfb1b2a40a1630a76ddd4c12
Lorenzo Bianconi <lorenzo@kernel.org>
2021-10-09 17:47:43 +0200

    nl80211: report offchannel radar/cac detection capability
    Report offchannel radar/cac detection capability if supported
    by the underlying hw.
    
    Tested-by: Owen Peng <owen.peng@mediatek.com>

--- src/drivers/driver.h
+++ src/drivers/driver.h
@@ -2025,6 +2025,8 @@ struct wpa_driver_capa {
 #define WPA_DRIVER_FLAGS2_OCV			0x0000000000000080ULL
 /** Driver expects user space implementation of SME in AP mode */
 #define WPA_DRIVER_FLAGS2_AP_SME		0x0000000000000100ULL
+/** Driver supports offchannel radar/CAC detection */
+#define WPA_DRIVER_RADAR_OFFCHAN		0x0000000000000200ULL
 	u64 flags2;
 
 #define FULL_AP_CLIENT_STATE_SUPP(drv_flags) \
