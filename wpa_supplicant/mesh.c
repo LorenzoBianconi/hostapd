546c032194b1c96291103d0a5d15aec13e111a13
Lorenzo Bianconi <lorenzo@kernel.org>
2023-07-03 18:57:01 +0200

    mesh noscan

--- wpa_supplicant/mesh.c
+++ wpa_supplicant/mesh.c
@@ -506,6 +506,8 @@ static int wpa_supplicant_mesh_init(struct wpa_supplicant *wpa_s,
 			   frequency);
 		goto out_free;
 	}
+	if (ssid->noscan)
+		conf->noscan = 1;
 
 	if (ssid->mesh_basic_rates == NULL) {
 		/*
